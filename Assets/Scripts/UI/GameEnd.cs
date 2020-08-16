2eda47060b0266c521bd74296e901aed81e7e85a
guolifeng2018 <guolifeng@linqsoft.com>
2020-08-16 15:30:07 +0800

    逻辑提交

---
+++ Assets/Scripts/UI/GameEnd.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour
+{
+    public GameObject m_gameEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_gameEnd.SetActive(false);
+
+        LeanTween.value(0, 1f, 2f).setOnComplete((o =>
+        {
+            m_gameEnd.SetActive(true);
+            LeanTween.value(0, 1f, 2f).setOnComplete(o1 =>
+            {
+                GameSceneManager.Instance.LoadScene("Start");
+            });
+        }));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
